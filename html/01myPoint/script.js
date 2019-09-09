d07cb55019157b84b0a4acdf8f50a323be088160
Firelonze <26387@ma-web.nl>
2019-09-09 12:19:47 +0200

    Challenges completed

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,29 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let points = [];
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    let colour = "rgba(" + getRandom(255) + "," + getRandom(255) + "," + getRandom(255) + ","+ "0.2" +")"
+    let A = new Point(new Vector2d(getRandom(width), getRandom(height)), getRandom(200), colour,"plant");
+    points.push(A);
+    for(let i = 0; i<points.length; i++){
+        points[i].label = i;
+        points[i].radius++;
+        points[i].draw(context);
+        if (points[i].radius >= 200){
+            points.splice(i,1);
+        }
+    }
+}
+
+animate();
+
+function getRandom(max){
+    let ans = Math.floor(Math.random()*max);
+    return ans;
+}
