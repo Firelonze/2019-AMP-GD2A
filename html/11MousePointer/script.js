b0c200ae533d3c6d93ac6ce1e1527cb241057618
Robin van den Bor <43145241+Firelonze@users.noreply.github.com>
2019-11-01 15:42:39 +0100

    11 added

---
+++ html/11MousePointer/script.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+let grid = new Grid();
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    context.clearRect(0,0,canvas.width,canvas.height);
+    context.moveTo(0,0);
+}
+
+animate();
